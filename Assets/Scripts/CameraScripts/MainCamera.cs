e846fbbe32d22375264862b21f91c5797a0d1329
Brian Lai <bhlai@andrew.cmu.edu>
2017-03-01 22:15:41 -0500

    added camera smooth follow

---
+++ Assets/Scripts/CameraScripts/MainCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	private float dampTime = 0.75f;
+
+	public float DampTime {
+		get { return dampTime; } 
+		set { dampTime = value; } 
+	}
+		
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
+	private Camera camera;
+
+	void Start(){
+		camera = gameObject.GetComponent<Camera> ();
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+}
