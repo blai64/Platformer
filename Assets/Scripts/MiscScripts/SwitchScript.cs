f7985dce617c8e7848ddd26faf984884ed1341b8
Brian Lai <bhlai@andrew.cmu.edu>
2017-03-05 14:28:20 -0500

    added platform switching

---
+++ Assets/Scripts/MiscScripts/SwitchScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour {
+	//TODO: Need to add animations for switch object
+
+	private bool isActive;
+
+	public bool IsActive{
+		get{ return isActive;}
+		set{ isActive = value;}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+	}
+		
+	//If player is colliding and presses button, then change switch
+	void OnTriggerStay (Collider other){
+		if (other.gameObject.CompareTag ("Player") && Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log("Switching");
+			isActive = !isActive;
+		}
+			
+	}
+}
---
+++ Assets/Scripts/MiscScripts/SwitchTarget.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Abstract class for all things that could be a target of a switch
+public class SwitchTarget : MonoBehaviour {
+
+	public GameObject mySwitch;
+	private bool isActive;
+	private bool switchActive;
+
+	// Use this for initialization
+	void Start () {
+		isActive = mySwitch.GetComponent<SwitchScript> ().IsActive;
+	}
+	
+	//Check if switch active status is the same, otherwise 
+	virtual public void Update () {
+		switchActive = mySwitch.GetComponent<SwitchScript> ().IsActive;
+		if (isActive != switchActive) {
+			Debug.Log ("switch changed!");
+			isActive = switchActive;
+			Activate (isActive);
+		}
+	}
+
+	//override to determine exactly what the switch target should be doing, can 
+	//extend to different things (moving spikes up and down, 
+	virtual public void Activate(bool activating){
+		Debug.Log ("hello");
+	}
+}
