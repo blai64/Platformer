ce5b1a18c879070a68bc60d09bb8587b99ce6e83
楼早 <Mabel@louzaos-MBP.wv.cc.cmu.edu>
2017-03-06 17:26:21 -0500

    main

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -87,10 +87,7 @@ public class PlayerScript : MonoBehaviour {
 				anim.SetBool ("isJumpingRight", true);
 			} else
 				anim.SetBool ("isJumpingLeft", true);
-<<<<<<< HEAD
 			jumping = true;
-=======
->>>>>>> 7cdff69584c1767044897428f60983eda1e839e1
 			//direction = 1;
 			//transform.Translate (0, .1f * direction, 0);
 			body.AddForce (Vector3.up * jumpForce);
