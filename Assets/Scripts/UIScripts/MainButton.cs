b07494aa4f79c4d2e7d726416a3431f402563385
Michelle Ma <michell3@andrew.cmu.edu>
2017-03-22 12:08:15 -0400

    Zao's UI Stuff

---
+++ Assets/Scripts/UIScripts/MainButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainButton : MonoBehaviour {
+	public Button yourButton;
+	public GameObject menu;
+	public string nextScene;
+	private Renderer render;
+	private bool show;
+
+	// Use this for initialization
+	void Start () {
+		//render = menu.GetComponent<Renderer> ();
+		//render.enabled = false;
+		menu.SetActive(false);
+		//witch.SetActive (true);
+		//menu.SetActive (false);
+		show = menu.activeSelf;
+		Debug.Log (show);
+
+			//Button btn = yourButton.GetComponent<Button> ();
+			//btn.onClick.AddListener (Update);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		show = menu.activeSelf;
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			yourButton.Select ();
+			if (show == true)
+				menu.SetActive (false);
+			else
+				menu.SetActive (true);
+			//render.enabled = true;
+			//SceneManager.LoadScene (nextScene);
+		}
+	}
+}
---
+++ Assets/Scripts/UIScripts/NormalButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NormalButton : MonoBehaviour {
+	public Button yourButton;
+	public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = yourButton.GetComponent<Button> ();
+		btn.onClick.AddListener (Update);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Main is pressed????!!!!");
+		SceneManager.LoadScene (nextScene);
+	}
+}
